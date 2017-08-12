7b7a1f00d5956a3e56f6f364d47f083ce5338b2f
“Klammertime” <audrey.ck@gmail.com>
2017-08-11 21:00:36 -0700

    Added SASS

---
+++ NOTES.md
@@ -0,0 +1,14 @@
+Besides writing code, need:
+- minification
+- concatenation
+- vendor prefixes
+- sass to css compilation
+- optimizing 3rd party and custom code
+- injecting files into HTML
+- file revisions and versioning
+- caching
+- testing
+- code analysis (jshint)
+Gulp uses streams, like a pipeline.
+
+In converting project, you'll want to take advantage of variables. One of easiest ways is converting colors, by doing a visual analysis of colors used.
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 "use strict";
 var gulp = require('gulp'),
     del = require('del'),
-    pages = require('gulp-gh-pages');
+    pages = require('gulp-gh-pages'),
+    sass = require('gulp-sass'),
+    csscss = require('gulp-csscss'),
+    deleteUnusedImages = require('gulp-delete-unused-images'),
+    plumber = require('gulp-plumber'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
+
+var $ = require('gulp-load-plugins')({
+    lazy: true
+});
 
 var options = {
     dist: './dist/'
@@ -12,24 +22,52 @@ gulp.task('clean', function() {
     del(['dist']);
 });
 
-gulp.task("build", function() {
-    return gulp.src([
-            "index.html",
-            "event-form-signup.html",
-            "images/*",
-            "css/*"
-        ], {
-            base: './'
-        })
-        .pipe(gulp.dest('dist'));
+gulp.task("deleteImages", function() {
+return gulp.src(['assets/images/*', 'index.html'])
+  .pipe(plumber())
+  .pipe(deleteUnusedImages({
+    log: true,
+    delete: true
+  }))
+    .pipe(plumber.stop());
 });
+
+gulp.task('redundantCss', function() {
+  gulp.src('assets/css/main.css')
+    .pipe(csscss())
+});
+
+gulp.task('minifyCSS', function(){
+  gulp.src([
+    "assets/css/main.css",
+    "assets/css/normalize.css"
+    ])
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest("assets/css"));
+});
+
+gulp.task("build", ['minifyCSS'], function(){
+  return gulp.src([
+    "assets/css/main.min.css",
+    "assets/css/normalize.min.css",
+    "index.html",
+    "event-form-signup.html",
+    "assets/images/**"], { base: "./"})
+      .pipe(gulp.dest("dist"));
+});
+
+gulp.task('serve', ['watchFiles']);
 
 gulp.task('deploy', function() {
     return gulp.src(options.dist + '**/**/*')
         .pipe(pages());
-})
+});
 
-// Build task is a dependency of default task so can run command "gulp".
 gulp.task('default', ['clean'], function() {
     gulp.start('build')
 });
+
+
+
+
